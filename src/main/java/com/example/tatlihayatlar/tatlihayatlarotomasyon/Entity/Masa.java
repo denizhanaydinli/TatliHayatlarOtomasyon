7f281b02266a6fed3dcd884cc8f991db706feca7
denizhanayinli <daydinli93@gmail.com>
2023-12-04 17:44:20 +0300

    Masa butonuna tıkladığımızda sağ tarfta kontol paneli çıkması eklendi

--- src/main/java/com/example/tatlihayatlar/tatlihayatlarotomasyon/Entity/Masa.java
+++ src/main/java/com/example/tatlihayatlar/tatlihayatlarotomasyon/Entity/Masa.java
@@ -1,12 +1,17 @@
 package com.example.tatlihayatlar.tatlihayatlarotomasyon.Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Masa {
     private int masaNo;
     private boolean dolu;
+    private List<String> adisyon;
 
     public Masa(int masaNo, boolean dolu) {
         this.masaNo = masaNo;
         this.dolu = dolu;
+        this.adisyon = new ArrayList<>();
     }
 
     public int getMasaNo() {
@@ -23,5 +28,16 @@ public class Masa {
 
     public void setDolu(boolean dolu) {
         this.dolu = dolu;
+    }
+    public List<String> getAdisyon() {
+        return adisyon;
+    }
+
+    public void setAdisyon(List<String> adisyon) {
+        this.adisyon = adisyon;
+    }
+
+    public void adisyonEkle(String urun) {
+        this.adisyon.add(urun);
     }
 }
--- src/main/java/com/example/tatlihayatlar/tatlihayatlarotomasyon/HelloApplication.java
+++ src/main/java/com/example/tatlihayatlar/tatlihayatlarotomasyon/HelloApplication.java
@@ -11,8 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseButton;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Modality;
@@ -25,10 +24,14 @@ import java.util.List;
 
 public class HelloApplication extends Application {
 
+
     private static final int MAX_MASALAR = 20;
     private static final int MASALARI_SIRALA = 5;
 
     private final UrunListesi urunListesi = new UrunListesi();
+
+    private ListView<String> urunListView = new ListView<>();
+    private VBox masaBilgisiPanel = new VBox();
 
     @Override
     public void start(Stage primaryStage) {
@@ -47,6 +50,11 @@ public class HelloApplication extends Application {
             gridPane.add(masaButton, i % MASALARI_SIRALA, i / MASALARI_SIRALA + 1);
         }
 
+        // Sağ tarafta gösterilecek olan Masa Bilgisi Paneli
+        masaBilgisiPanel.setAlignment(Pos.TOP_LEFT);
+        masaBilgisiPanel.setSpacing(10);
+        masaBilgisiPanel.setPadding(new Insets(10));
+
         // Sipariş fiyat listesi butonu
         Button fiyatListesiButton = new Button("Sipariş Fiyat Listesi");
         fiyatListesiButton.setOnAction(e -> {
@@ -54,7 +62,11 @@ public class HelloApplication extends Application {
         });
         gridPane.add(fiyatListesiButton, 0, MAX_MASALAR / MASALARI_SIRALA + 2, 2, 1);
 
-        Scene scene = new Scene(gridPane, 800, 600);
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(gridPane);
+        borderPane.setRight(masaBilgisiPanel);
+
+        Scene scene = new Scene(borderPane, 800, 600);
 
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -66,39 +78,26 @@ public class HelloApplication extends Application {
 
         // Masa tıklanabilir olduğunda gerçekleşecek olayları tanımla
         masaButton.setOnAction(e -> {
-            if (masa.isDolu()) {
-                // Masa zaten doluysa popup ekranı açma
-                System.out.println("Masa " + masa.getMasaNo() + " zaten dolu!");
-                showPopup(masa);  // Masa doluysa, sipariş bilgisini göster
-            } else {
-                // Masa boşsa uyarı ver
-                showAlert("Masa " + masa.getMasaNo() + " boş. Lütfen önce bir sipariş girin.");
-            }
+            showMasaBilgisi(masa);
         });
 
         return masaButton;
     }
 
-    private void showPopup(Masa masa) {
-        Stage popupStage = new Stage();
-        popupStage.initModality(Modality.APPLICATION_MODAL);
-        popupStage.setTitle("Sipariş Bilgisi");
+    private void showMasaBilgisi(Masa masa) {
+        masaBilgisiPanel.getChildren().clear();
 
-        GridPane popupGrid = new GridPane();
-        popupGrid.setAlignment(Pos.CENTER);
-        popupGrid.setHgap(10);
-        popupGrid.setVgap(10);
-        popupGrid.setPadding(new Insets(20, 20, 20, 20));
+        Label masaBilgisiLabel = new Label("Masa Bilgisi - Masa " + masa.getMasaNo());
+        masaBilgisiLabel.setStyle("-fx-font-weight: bold; -fx-font-size: 16px;");
 
-        // Masaya ait ürün adı ve fiyatını getir
+        // Masaya ait sipariş bilgilerini getir
         String masaUrunAdi = "";  // Masa için kaydedilmiş ürün adı
         double masaUrunFiyati = 0.0;  // Masa için kaydedilmiş ürün fiyatı
 
         // Eğer masa dolu ise bilgileri çek
-        if (masa.isDolu()) {
+        try {
             // Masa ile ilgili bilgileri çek
             // Bu kısımda ilgili veritabanı sorgularını kullanmalısınız.
-            // Örneğin, "SELECT * FROM siparisler WHERE masa_id = ?" gibi bir sorgu kullanabilirsiniz.
             // resultSet'den masaUrunAdi ve masaUrunFiyati'ni çekmelisiniz.
 
             // Örnek:
@@ -108,34 +107,48 @@ public class HelloApplication extends Application {
             //    masaUrunFiyati = resultSet.getDouble("urun_fiyati");
             // }
 
+            // Eğer veritabanı kullanıyorsanız, gerçek veritabanı sorgularını kullanmalısınız.
+            // Aşağıdaki satırlar sadece bir örnek olarak verilmiştir ve gerçek veritabanı yapınıza göre uyarlanmalıdır.
+
+            masaUrunAdi = "Ürün adı";  // Örnek değer, gerçek veritabanı değeri ile değiştirilmeli
+            masaUrunFiyati = 20.0;  // Örnek değer, gerçek veritabanı değeri ile değiştirilmeli
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            showAlert("Sipariş bilgileri alınırken bir hata oluştu.");
         }
 
         // Ürün Adı ve Fiyat giriş alanları
-        Label urunAdiLabel = new Label("Ürün Adı:");
-        TextField urunAdiField = new TextField(masaUrunAdi);
-        urunAdiField.setEditable(false);  // Kullanıcıya düzenleme izni verme
-        Label fiyatLabel = new Label("Fiyat:");
-        TextField fiyatField = new TextField(String.valueOf(masaUrunFiyati));
-        fiyatField.setEditable(false);  // Kullanıcıya düzenleme izni verme
+        Label urunAdiLabel = new Label("Ürün Adı: " + masaUrunAdi);
+        Label fiyatLabel = new Label("Fiyat: " + masaUrunFiyati);
 
-        popupGrid.add(urunAdiLabel, 0, 0);
-        popupGrid.add(urunAdiField, 1, 0);
-        popupGrid.add(fiyatLabel, 0, 1);
-        popupGrid.add(fiyatField, 1, 1);
+        HBox urunBilgisiBox = new HBox(urunAdiLabel, new Separator(), fiyatLabel);
+        urunBilgisiBox.setAlignment(Pos.CENTER_LEFT);
+        urunBilgisiBox.setSpacing(10);
 
-        // Kaydet butonu
-        Button kaydetButton = new Button("Kaydet");
-        kaydetButton.setOnAction(event -> {
-            // Eğer gerekirse, burada başka işlemler de yapabilirsiniz.
-            // Örneğin, yeni bir sipariş oluşturabilir ve veritabanına ekleyebilirsiniz.
-            showAlert("Sipariş başarıyla kaydedildi.");
-            popupStage.close();
-        });
-        popupGrid.add(kaydetButton, 1, 2);
+        masaBilgisiPanel.getChildren().addAll(masaBilgisiLabel, urunBilgisiBox);
 
-        Scene popupScene = new Scene(popupGrid, 300, 200);
-        popupStage.setScene(popupScene);
-        popupStage.showAndWait();
+        // Ürün listesini ve butonları ekle
+        VBox urunlerVBox = new VBox();
+        urunlerVBox.setSpacing(10);
+        urunlerVBox.setAlignment(Pos.TOP_LEFT);
+
+        try {
+            List<String> urunAdlari = urunListesi.getUrunListesiAdlari();
+            for (String urunAdi : urunAdlari) {
+                Button urunButton = new Button(urunAdi);
+                urunButton.setOnAction(event -> {
+                    // Ürün butonuna tıklandığında yapılacak işlemleri buraya ekleyebilirsiniz.
+                    showAlert("Ürün seçildi: " + urunAdi);
+                });
+                urunlerVBox.getChildren().add(urunButton);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            showAlert("Ürün listesi alınırken bir hata oluştu.");
+        }
+
+        masaBilgisiPanel.getChildren().add(urunlerVBox);
     }
 
     private void showUrunListesiPopup() {
@@ -149,8 +162,6 @@ public class HelloApplication extends Application {
         urunListesiGrid.setVgap(10);
         urunListesiGrid.setPadding(new Insets(20, 20, 20, 20));
 
-        // Eski ürün adı ve fiyatları gösteren alan
-        ListView<String> urunListView = new ListView<>();
         urunListView.setPrefSize(200, 200);
 
         try {
@@ -163,7 +174,6 @@ public class HelloApplication extends Application {
 
         urunListView.setOnMouseClicked(event -> {
             if (event.getClickCount() == 2) {
-                // Çift tıklama olayı
                 String selectedUrun = urunListView.getSelectionModel().getSelectedItem();
                 if (selectedUrun != null) {
                     urunListesi.urunSil(selectedUrun);
@@ -175,7 +185,6 @@ public class HelloApplication extends Application {
 
         urunListesiGrid.add(urunListView, 0, 0);
 
-        // Ürün Adı ve Fiyat giriş alanları
         TextField urunAdiField = new TextField();
         TextField fiyatField = new TextField();
 
@@ -184,16 +193,10 @@ public class HelloApplication extends Application {
         urunListesiGrid.add(new Label("Fiyat:"), 1, 1);
         urunListesiGrid.add(fiyatField, 2, 1);
 
-        // Kaydet, Güncelle, Sil butonları
         Button kaydetButton = new Button("Kaydet");
-        Button guncelleButton = new Button("Güncelle");
-        Button silButton = new Button("Sil");
 
         urunListesiGrid.add(kaydetButton, 1, 2);
-        urunListesiGrid.add(guncelleButton, 2, 2);
-        urunListesiGrid.add(silButton, 3, 2);
 
-        // Kaydet butonu
         kaydetButton.setOnAction(event -> {
             String urunAdi = urunAdiField.getText();
             String fiyatStr = fiyatField.getText();
@@ -207,18 +210,6 @@ public class HelloApplication extends Application {
                 showAlert("Lütfen tüm alanları doldurun.");
             }
         });
-        silButton.setOnAction(event -> {
-            String selectedUrun = urunListView.getSelectionModel().getSelectedItem();
-            if (selectedUrun != null) {
-                urunListesi.urunSil(selectedUrun);
-                refreshUrunListesi(urunListView);
-                showAlert("Ürün başarıyla silindi.");
-            } else {
-                showAlert("Lütfen silinecek bir ürün seçin.");
-            }
-        });
-
-        // ... (diğer kodlar)
 
         Scene urunListesiScene = new Scene(urunListesiGrid, 500, 300);
         urunListesiStage.setScene(urunListesiScene);
@@ -235,7 +226,6 @@ public class HelloApplication extends Application {
             showAlert("Ürün listesi alınırken bir hata oluştu.");
         }
     }
-
 
     private void showAlert(String message) {
         Alert alert = new Alert(Alert.AlertType.WARNING);
